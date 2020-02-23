be1cb910e2edfe5603b647c6529da4b9a1229cf4
Dat Phung <tiendat.phung11@gmail.com>
2020-02-23 21:33:57 +0700

    static file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+app.use(express.static('public'));
 //Get request to get data from root
 app.get('/', function(req, res){
     res.render('index', {
