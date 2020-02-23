fa9705a8e55b3d4e4928c49aafdc7bcfab76a05e
Dat Phung <tiendat.phung11@gmail.com>
2020-02-23 15:21:58 +0700

    install and use template engines

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-//Get request to root
-app.get('/', function(request, response){
-    response.send('<h1>Hello World</h1>')
+app.set('view engine', 'pug');
+app.set('views', './views');
+//Get request to get data from root
+app.get('/', function(req, res){
+    res.render('index', {
+        name: 'Dat'
+    });
 });
-//Get request to /users
-app.get('/users', function(request, response){
-    response.send('User list')
+//Get request to get data from users
+app.get('/users', function(req, res){
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Dat' },
+            { id: 2, name: 'Huong' }
+        ]
+    });
 });
 app.listen(port, function(){
     console.log('Server listening on port ' + port);
