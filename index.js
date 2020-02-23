62d7b2bf15f79fdc70bab0d2b4a5829cb0d81fa2
Dat Phung <tiendat.phung11@gmail.com>
2020-02-23 16:29:15 +0700

    search by query

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
+var users = [
+    { id: 1, name: 'Dat' },
+    { id: 2, name: 'Huong' }
+];
 //Get request to get data from root
 app.get('/', function(req, res){
     res.render('index', {
@@ -12,10 +16,16 @@ app.get('/', function(req, res){
 //Get request to get data from users
 app.get('/users', function(req, res){
     res.render('users/index', {
-        users: [
-            { id: 1, name: 'Dat' },
-            { id: 2, name: 'Huong' }
-        ]
+        users: users
+    });
+});
+app.get('/users/search', function(req, res){
+    var q = req.query.q; // req.query is object & q is key
+    var matchedUsers = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render('users/index', {
+        users: matchedUsers
     });
 });
 app.listen(port, function(){
