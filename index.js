e3e0d35b7fecdc3a1d1b6dea1c9c179dadf055b8
Dat Phung <tiendat.phung11@gmail.com>
2020-02-23 17:48:58 +0700

    config lowdb

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+db = low(adapter);
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write();
 var app = express();
 var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-var users = [
-    { id: 1, name: 'Dat' },
-    { id: 2, name: 'Huong' }
-];
+
 //Get request to get data from root
 app.get('/', function(req, res){
     res.render('index', {
@@ -18,12 +22,12 @@ app.get('/', function(req, res){
 //Get request to get data from users
 app.get('/users', function(req, res){
     res.render('users/index', {
-        users: users
+        users: db.get('users').value()
     });
 });
 app.get('/users/search', function(req, res){
     var q = req.query.q; // req.query is object & q is key
-    var matchedUsers = users.filter(function(user){
+    var matchedUsers = db.get('users').value().filter(function(user){
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
     res.render('users/index', {
@@ -36,7 +40,7 @@ app.get('/users/create', function(req, res){
 });
 // post request
 app.post('/users/create', function(req, res){
-    users.push(req.body); // res.body đọc dữ liệu từ client gửi lên và chuyển sang object rồi lưu vào req.body
+    db.get('users').push(req.body).write(); // res.body đọc dữ liệu từ client gửi lên và chuyển sang object rồi lưu vào req.body
     res.redirect('/users');// điều hướng sau khi create sang trang users
 });
 app.listen(port, function(){
