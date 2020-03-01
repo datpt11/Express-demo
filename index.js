a55795a692355d6c426767889246555a9c7a99c4
Dat Phung <tiendat.phung11@gmail.com>
2020-03-01 14:51:08 +0700

    add to cart with session

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var app = express();
 var userRoute = require('./routes/user.route')
 var authRoute = require('./routes/auth.route');
 var productRoute = require('./routes/product.route');
+var carRoute = require('./routes/cart.route');
 var authMiddleware = require('./middlewares/auth.middleware');
+var sessionMiddleware = require('./middlewares/session.middleware');
 var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -13,6 +15,7 @@ app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
+app.use(sessionMiddleware);
 //Get request to get data from root
 app.get('/', function(req, res){
     res.render('index', {
@@ -22,6 +25,7 @@ app.get('/', function(req, res){
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
 app.use('/products', productRoute);
+app.use('/cart', carRoute);
 app.listen(port, function(){
     console.log('Server listening on port ' + port);
 });
